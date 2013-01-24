869e942f4d7f795c6114af3d7a2acffdf6a1d767
raa0121 <raa0121@gmail.com>
2013-01-24 13:59:55 +0900

    load DodontoFServer

--- web.rb
+++ web.rb
@@ -1,7 +1,12 @@
-require 'sinatra'
+$LOAD_PATH << File.dirname(__FILE__) + "/DodontoF"
+load 'DodontoFServer'
+require 'sinatra/base'
 
-post '/DodontoF/DodontoF.swf'
-  send_file File.expand_path("DodontoF",__FILE__),
-      :type => 'application/x-shockwave-flash', :dispositon => 'inline' \
+class DodontoFApp < Sinatra::Base
+  
+  post '/DodontoF/DodontoF.swf' do
+    send_file File.expand_path("DodontoF",__FILE__),
+        :type => 'application/x-shockwave-flash', :dispositon => 'inline' \
       rescue raise(Sinatra::NotFound)
+  end
 end
